814a418b0e025b8ff872cfde26d932420dcdbb7f
Dima_V <astrallux777@ukr.net>
2025-05-28 15:08:07 +0300

    28.05.2025-DZ-09-03

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,35 @@
-console.log('Form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+console.log(form);
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+const textarea = document.querySelector('textarea');
+
+textarea.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  const message = event.target.value;
+  console.dir(event.target);
+
+  console.log(message);
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function textInput() {
+  const message = localStorage.getItem(STORAGE_KEY);
+  if (message) {
+    textarea.value = message;
+  }
+}
+
+textInput();
